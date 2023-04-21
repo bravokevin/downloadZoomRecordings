80d2a70011b5b2034e86f3258f81ded7db225d83
Kevin Bravo <bravokevinto@gmail.com>
2023-04-21 13:31:44 -0400

    refactor: change layout and refactoring functions

--- downloadRecordings.js
+++ downloadRecordings.js
@@ -1,107 +1,55 @@
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-const fs = require('fs');
 
-const zoomApiKey = "OWY3qI3gSbWPgxIyyjxn_A";
-const zoomApiSecret = "1BOJ9agAAmYVMsYr3eEFpbrCLdEEXlvHFdwZ";
-const userId = 'programa.proexcelencia@gmail.com';
-const oneDriveAccessToken = 'YOUR_ONEDRIVE_ACCESS_TOKEN';
-
-const payload = {
-    iss: zoomApiKey,
-    exp: ((new Date()).getTime() + 5000)
-};
-const token = jwt.sign(payload, zoomApiSecret);
-
-async function listRecordings(from, to) {
-    console.log(`+++++++++++++++++++ Listando grabaciones desde ${from} hasta ${to} +++++++++++++++++++`)
-    const response = await axios.get(`https://api.zoom.us/v2/users/${userId}/recordings?from=${from}&to=${to}`, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    return response.data.meetings;
-}
-
-async function downloadRecording(recording, topic) {
-    try {
-        console.log("Descargando: " + topic)
-        const response = await axios.get(recording.download_url, {
-            responseType: 'stream',
-            timeout: 60000,// 60 seconds
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        console.log("Descarga Finalizada de: " + topic)
-
-        const folder = 'videos';
-        if (!fs.existsSync(folder)) {
-            fs.mkdirSync(folder);
-        }
-        const writer = fs.createWriteStream(`${folder}/${topic}.mp4`);
-        response.data.pipe(writer);
-        await new Promise((resolve, reject) => {
-            writer.on('finish', resolve);
-            writer.on('error', reject);
-        });
-
-        const stats = fs.statSync(`${folder}/${topic}.mp4`);
-        if (stats.size !== recording.file_size) {
-            console.error(`Downloaded file size does not match expected size for recording ${recording.id}`);
-            // retry download
-            await downloadRecording(recording, topic);
-        }
-
-    } catch (error) {
-        console.error(`Error downloading recording ${recording.id}: ${error.message}`);
-        // retry download
-        await downloadRecording(recording, topic);
-    }
-}
-
-
-async function uploadToOneDrive(filePath) {
-    const fileName = path.basename(filePath);
-    const fileStream = fs.createReadStream(filePath);
-    const fileSize = fs.statSync(filePath).size;
-    const response = await axios.put(`https://graph.microsoft.com/v1.0/me/drive/root:/${fileName}:/content`, fileStream, {
-        headers: {
-            'Authorization': `Bearer ${oneDriveAccessToken}`,
-            'Content-Length': fileSize
-        }
-    });
-    return response.data;
-}
-
-async function main() {
-    let from = new Date('2020-01-01');
-    let to = new Date(from);
+const main = async () => {
+    let fromWhitNoFormat = new Date('2020-01-01');
+    let toWhitNoFormat = new Date(fromWhitNoFormat);
     to.setMonth(to.getMonth() + 1);
-    while (from < new Date()) {
-        const recordings = await listRecordings(from.toISOString().split('T')[0], to.toISOString().split('T')[0]);
-        for (const recording of recordings) {
-            for (const file of recording.recording_files) {
-                if (file.file_type === 'MP4') {
-                    const filePath = `videos/${recording.topic}.mp4`;
+
+    const now = new Date();
+
+    while (from < now) {
+        const from = fromWhitNoFormat.toISOString().split('T')[0]
+        const to = toWhitNoFormat.toISOString().split('T')[0]
+
+        const recordings = await listRecordings('2023-04-01', '2023-05-01', false);
+        if (recordings.length === 0) {
+            console.log(YELLOW_LOG_COLOR, `+++++++++++++++++++ No se encontraron grabaciones en el rango +++++++++++++++++++ \n`)
+        }
+        else {
+            console.log(CYAN_LOG_COLOR, `+++++++++++++++++++ Se encontraron ${recordings.length} grabaciones en el rango +++++++++++++++++++ \n`)
+            return;
+        }
+
+        recordings.forEach(recording = async () => {
+            const files = recording.recording_files
+            const name = recording.topic
+            const fileType = file.file_type;
+
+            if (files.length === 0) {
+                console.log(YELLOW_LOG_COLOR, `+++++++++++++++++++ No se encontraron archivos en ${name} +++++++++++++++++++ \n`)
+            }
+            else {
+                if (fileType === 'MP4') {
                     await downloadRecording(file, recording.topic);
-                    // await deleteRecording(file.id);
-                    // await uploadToOneDrive(filePath);
+                    const filePath = `videos/${recording.topic}.mp4`;
+                    await uploadToOneDrive(filePath);
                 }
             }
-        }
+            // const filesInSys = fs.readdirSync("videos");
+            // const fileNames = filesInSys.map(file => path.parse(file).name)        
+            // if (fileNames.includes(recording.topic)) {
+            // }
+            // else{
+            //     console.log("No se pudo descargar el archivo")
+            // }
+            await deleteRecording(recording.uuid);
+
+        })
+        //add 1 month to "to" and "from" to start 
         from.setMonth(from.getMonth() + 1);
         to.setMonth(to.getMonth() + 1);
+
     }
 }
-async function deleteRecording(recordingId) {
-    await axios.delete(`https://api.zoom.us/v2/recordings/${recordingId}`, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-}
-
 
 (async () => {
     await main()
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Utilities for colowing logs response. 
+ */
+
+export const CYAN_LOG_COLOR = '\x1b[36m%s\x1b[0m';
+export const RED_LOG_COLOR = '\x1b[31m%s\x1b[0m';
+export const YELLOW_LOG_COLOR = '\x1b[33m%s\x1b[0m';
+export const GREEN_LOG_COLOR = '\x1b[32m%s\x1b[0m';
